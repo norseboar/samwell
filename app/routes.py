4b3a51af55f8160b6c85b29cad540aec0604e7a6
Reed Molbak <reed@benchling.com>
2021-02-21 14:26:07 -0800

    Add support for call and response in threads

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,18 @@
 import logging
-
-from app import app
+import os
 
 from flask import request
+from slack_sdk import WebClient
+
+from app import app
+from .lib import decide_is_question
+
+REQUEST_TYPE_EVENT = 'event_callback'
+REQUEST_TYPE_VERIFICATION = 'url_verification'
+EVENT_TYPE_MESSAGE = 'message'
+BOT_USER_ID = os.environ['BOT_USER_ID']
+
+client = WebClient(token=os.environ['SLACK_BOT_TOKEN'])
 
 @app.route('/')
 def home():
@@ -14,11 +24,29 @@ def slack_events():
 
     request_type = data['type']
 
-    if request_type == 'url_verification':
+    if request_type == REQUEST_TYPE_VERIFICATION:
         app.logger.info('Received challenge')
         return data['challenge']
+
+    if request_type != REQUEST_TYPE_EVENT:
+        app.logger.info(data)
+        return {}
+    
+    event_type = data['event']['type']
+
+    if event_type == EVENT_TYPE_MESSAGE:
+        message_text = data['event']['text']
+
+        if "debug" in message_text:
+            app.logger.info(data)
+
+        app.logger.info('Received message {}'.format(message_text))
+        if decide_is_question(message_text) and data['event']['user'] != BOT_USER_ID:
+            thread_ts = data['event'].get('thread_ts') or data['event']['ts']
+            client.chat_postMessage(channel='#integration-test', text='Did somebody say question?', thread_ts=thread_ts)
+        return {}
     else:
-        app.logger.info(data['event']['text'])
+        app.logger.info(data)
         return {}
 
 
